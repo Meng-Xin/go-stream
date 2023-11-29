547ed11f47bcaf77548dfc6c493e37470f86c3cd
Meng-Xin <li997266754@gmail.com>
2023-11-29 15:15:00 +0800

    feat: 补充mapsStream 测试方法

--- maps_test.go
+++ maps_test.go
@@ -1,13 +1,10 @@
 package go_stream
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 func TestMaps(t *testing.T) {
 	data := map[string]int{"one": 1, "two": 2, "three": 3, "four": 4}
-	fmt.Printf("data original address %p\n", data)
+	t.Logf("data original address %p\n", data)
 	stream := OfMaps(data)
 
 	// 过滤出值大于2的键值对
@@ -21,10 +18,19 @@ func TestMaps(t *testing.T) {
 	})
 
 	// 收集结果
-	result := doubledStream.Collect()
-	if _, ok := result.(map[string]int); ok {
-		fmt.Println("类型转换成功")
-	}
-	fmt.Printf("data changed address %p\n", data)
-	fmt.Println("End Val", result) // 输出: map[three:6 four:8]
+	doubledStream.Collect(&data)
+	t.Logf("data changed address %p\n", data)
+	t.Log("End Val", data) // 输出: map[three:6 four:8]
+}
+
+func TestMapsStream_Sorted(t *testing.T) {
+	data := map[string]int{"a": 1, "d": 4, "c": 3, "b": 2}
+	stream := OfMaps(data)
+	t.Logf("data original address %p\n", data)
+	stream.Sorted(func(x int, y int) bool {
+		return (x - y) > 0
+	})
+	stream.Collect(&data)
+	t.Logf("data changed address %p\n", data)
+	t.Log("End Val", data)
 }
